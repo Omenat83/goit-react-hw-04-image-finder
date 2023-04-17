d7c8adb1334797f5a7ad62594408990b0d9dd5ed
Maryna K <110280285+Omenat83@users.noreply.github.com>
2023-04-17 13:21:11 +0300

    4 hw/ fixed image-finder
    fetch from gallery to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,91 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { LoadMoreBtn } from 'components/Button/Button';
+import { Loader } from 'components/Loader/Loader';
+
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { Div } from './App.styled';
 
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '33668245-88b2d78a431fcfde02e20361a';
+
 export default function App() {
   const [pictureName, setPictureName] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pictures, setPictures] = useState([]);
+  const [status, setStatus] = useState('idle');
 
-  // передача значення інпуту з Searchbar під час сабміту форми
+  // передача значеннь під час сабміту форми
   const onFormSubmit = pictureName => {
+    setCurrentPage(1);
     setPictureName(pictureName);
+    setPictures([]);
+  };
+
+  useEffect(() => {
+    if (pictureName === '') {
+      return;
+    }
+
+    setStatus('pending');
+
+    foo();
+
+    async function foo() {
+      try {
+        const data = await fetchImages(pictureName, currentPage);
+        if (data.totalHits === 0) {
+          setPictures([]);
+          setStatus('resolved');
+          return toast.error(
+            `Sorry, we didn't find picture including ${pictureName}`
+          );
+        }
+        if (currentPage === 1) {
+          setPictures(data.hits);
+        } else {
+          setPictures(prevPictures => [...prevPictures, ...data.hits]);
+        }
+
+        if (data.hits.length < 12) {
+          setStatus('idle');
+          return toast.warn(`Sorry, picture including ${pictureName} ended :(`);
+        }
+        setStatus('resolved');
+      } catch (error) {
+        toast.error('Sorry, something wrong. Try again later');
+      }
+    }
+  }, [pictureName, currentPage]);
+
+  // запит картинок
+  function fetchImages(query, page) {
+    const data = fetch(
+      `${API_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=12`
+    ).then(res => res.json());
+    return data;
+  }
+
+  // збільшуємо номер сторінки
+  const onLoadMore = () => {
+    setCurrentPage(prevState => prevState + 1);
   };
 
   return (
     <Div>
       <ToastContainer autoClose={2500} />
       <Searchbar onSubmit={onFormSubmit} />
-      <ImageGallery pictureFindName={pictureName}/>
+      {status === 'pending' && currentPage === 1 && <Loader />}
+      {pictures.length > 0 && (
+        <>
+          <ImageGallery pictures={pictures} />
+          {status === 'resolved' && <LoadMoreBtn onClick={onLoadMore} />}
+          {status === 'pending' && <Loader />}
+        </>
+      )}
     </Div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,78 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { LoadMoreBtn } from 'components/Button/Button';
-import { Loader } from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
 import { GalUl } from './ImageGallery.styled';
 
-const API_URL = 'https://pixabay.com/api/';
-const API_KEY = '33668245-88b2d78a431fcfde02e20361a';
-
-export default function ImageGallery({ pictureFindName}) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pictures, setPictures] = useState([]);
-  const [status, setStatus] = useState('idle');
+export default function ImageGallery({ pictures }) {
   const [showModal, setShowModal] = useState(false);
   const [largeImgURL, setLargeImgURL] = useState('');
-
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [pictureFindName]);
-
-  useEffect(() => {
-    if (pictureFindName === '') {
-      return;
-    }
-
-    setStatus('pending');
-
-    foo();
-
-    async function foo() {
-      try {
-        const data = await fetchImages(pictureFindName, currentPage);
-        if (data.totalHits === 0) {
-          setPictures([]);
-          setStatus('resolved');
-          return toast.error(
-            `Sorry, we didn't find picture including ${pictureFindName}`
-          );
-        }
-        if (currentPage === 1) {
-          setPictures(data.hits);
-        } else {
-          setPictures(prevPictures => [...prevPictures, ...data.hits]);
-        }
-
-        if (data.hits.length < 12) {
-          setStatus('idle');
-          return toast.warn(
-            `Sorry, picture including ${pictureFindName} ended :(`
-          );
-        }
-        setStatus('resolved');
-      } catch (error) {
-        toast.error('Sorry, something wrong. Try again later');
-      }
-    }
-  }, [pictureFindName, currentPage]);
-
-  // запит картинок
-  function fetchImages(query, page) {
-    console.log('fetch', query, page);
-    const data = fetch(
-      `${API_URL}?key=${API_KEY}&q=${query}&page=${page}&per_page=12`
-    ).then(res => res.json());
-    return data;
-  }
-
-  // збільшуємо номер сторінки
-  const onLoadMore = () => {
-    setCurrentPage(prevState => prevState + 1);
-  };
 
   // обробка кліку по картинці, по id
   const onImgClick = e => {
@@ -92,29 +26,22 @@ export default function ImageGallery({ pictureFindName}) {
 
   return (
     <>
-      {status === 'pending' && currentPage === 1 && <Loader />}
       {showModal && <Modal largeImageURL={largeImgURL} onClose={toggleModal} />}
-      {pictures.length > 0 && (
-        <>
-          <GalUl onClick={e => onImgClick(e)}>
-            {pictures.map(picture => {
-              return (
-                <ImageGalleryItem
-                  key={picture.id}
-                  webformatURL={picture.webformatURL}
-                  id={picture.id}
-                />
-              );
-            })}
-          </GalUl>
-          {status === 'resolved' && <LoadMoreBtn onClick={onLoadMore} />}
-          {status === 'pending' && <Loader />}
-        </>
-      )}
+      <GalUl onClick={e => onImgClick(e)}>
+        {pictures.map(picture => {
+          return (
+            <ImageGalleryItem
+              key={picture.id}
+              webformatURL={picture.webformatURL}
+              id={picture.id}
+            />
+          );
+        })}
+      </GalUl>
     </>
   );
 }
 
 ImageGallery.propTypes = {
-  pictureFindName: PropTypes.string.isRequired,
+  pictures: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
